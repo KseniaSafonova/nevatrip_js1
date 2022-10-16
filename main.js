9a3d98e7dc13720e0f88f66f8ff8dc1bdc755fe8
KseniaSafonova <kseniatsn@gmail.com>
2022-10-16 18:47:53 +0300

    select

--- main.js
+++ main.js
@@ -2,50 +2,112 @@ let events = [{
     event_id: '001',
     ticket_adult_price: 500,
     ticket_kid_price: 300,
-    ticket_exemption_price: 250,
-    ticket_group_price: 200
+    ticket_exemption_price: 0,
+    ticket_group_price: 200,
+    ticket_type1_price: 0,
+    ticket_type2_price: 0
 },
 {
     event_id: '002',
     ticket_adult_price: 700,
     ticket_kid_price: 500,
     ticket_exemption_price: 300,
-    ticket_group_price: 250
+    ticket_group_price: 250,
+    ticket_type1_price: 0,
+    ticket_type2_price: 0
 },
 {
     event_id: '003',
     ticket_adult_price: 900,
     ticket_kid_price: 700,
-    ticket_exemption_price: 450,
-    ticket_group_price: 400
+    ticket_exemption_price: 0,
+    ticket_group_price: 400,
+    ticket_type1_price: 450,
+    ticket_type2_price: 400
 }
+]
+
+let users = [
+    {
+        user_id: '0001',
+        surname: 'Фамилия',
+        name: 'Имя'
+    }
 ]
 
 let Orders = [];
 
+
 document.addEventListener("DOMContentLoaded", function (event) {
+
     let restoredOrders = JSON.parse(localStorage.getItem("Orders"));
 
-    for (let order of restoredOrders) {
-        document.getElementById("trips").innerHTML +=
-            `<div class="trip">
-            <div class="tripSection">${order.id}</div>
-            <div class="tripSection">${trip.country}</div>
-            <div class="tripSection">${trip.city}</div>
-            <button class="tripSectionButton" onclick="openDetails()" id=${trip.id}>>>details</button>
-            <button class="deleteTrip" onclick="deleteTrip('${trip.id}');">delete</button>
-            </div>`
+    if ((restoredOrders != null) && (restoredOrders.length != 0)) {
+        for (i = 0; i < restoredOrders.length; i++) {
+            Orders.push(restoredOrders[i]);
+        }
+
+        for (let order of restoredOrders) {
+            document.querySelector('.table').innerHTML +=
+                `<tr>
+            <td>${order.id}</td>
+            <td>${order.event_id}</td>
+            <td>${order.event_date}</td>
+            <td>${order.ticket_adult_price}</td>
+            <td>${order.ticket_adult_quantity}</td>
+            <td>${order.ticket_kid_price}</td>
+            <td>${order.ticket_kid_quantity}</td>
+            <td>${order.ticket_exemption_price}</td>
+            <td>${order.ticket_exemption_quantity}</td>
+            <td>${order.ticket_group_price}</td>
+            <td>${order.ticket_group_quantity}</td>
+            <td>${order.ticket_type1_price}</td>
+            <td>${order.ticket_type1_quantity}</td>
+            <td>${order.ticket_type2_price}</td>
+            <td>${order.ticket_type2_quantity}</td>
+            <td>${order.barcode}</td>
+            <td>${order.user_id}</td>
+            <td>${order.equal_price}</td>
+            <td>${order.created}</td>
+        </tr>`
+        }
     }
 });
 
 let eventId = document.getElementById('eventId');
+let exemptionTicket = document.getElementById('exemptionTicket');
+let type1Ticket = document.getElementById('type1Ticket');
+let type2Ticket = document.getElementById('type2Ticket');
 let eventTime;
 
 eventId.addEventListener('change', function () {
+
+    let times = document.getElementsByClassName('select');
+
+    for (let time of times) {
+        time.style.display = 'none';
+    }
+
     let getValue = this.value;
     eventTime = document.getElementById('eventTime' + getValue);
     eventTime.style.display = 'block';
+
+    if (getValue == '002') {
+        exemptionTicket.style.display = 'block';
+        type1Ticket.style.display = 'none';
+        type2Ticket.style.display = 'none';
+    }
+    if (getValue == '003') {
+        exemptionTicket.style.display = 'none';
+        type1Ticket.style.display = 'block';
+        type2Ticket.style.display = 'block';
+    }
+
 });
+
+function closeList() {
+    eventTime.style.display = 'none';
+}
 
 function setStorageOrders() {
     localStorage.setItem("Orders", JSON.stringify(Orders));
@@ -60,7 +122,8 @@ function buyTickets() {
     let ticket_kid_quantity = document.getElementById('child').value;
     let ticket_exemption_quantity = document.getElementById('exemption').value;
     let ticket_group_quantity = document.getElementById('group').value;
-
+    let ticket_type1_quantity = document.getElementById('type1').value;
+    let ticket_type2_quantity = document.getElementById('type2').value;
 
     for (let event of events) {
         if (event_id == Object.values(event)[0]) {
@@ -68,6 +131,8 @@ function buyTickets() {
             kidPrice = event.ticket_kid_price;
             exemptionPrice = event.ticket_exemption_price;
             groupPrice = event.ticket_group_price;
+            type1Price = event.ticket_type1_price;
+            type2Price = event.ticket_type2_price;
         }
     }
 
@@ -83,8 +148,19 @@ function buyTickets() {
         ticket_exemption_quantity: +0 + +ticket_exemption_quantity,
         ticket_group_price: groupPrice,
         ticket_group_quantity: +0 + +ticket_group_quantity,
+        ticket_type1_price: type1Price,
+        ticket_type1_quantity: +0 + +ticket_type1_quantity,
+        ticket_type2_price: type2Price,
+        ticket_type2_quantity: +0 + +ticket_type2_quantity,
         barcode: (Math.random() * (1000000 - 0) + 0).toFixed(0),
-        equal_price: (ticket_adult_quantity * adultPrice) + (ticket_kid_quantity * kidPrice) + (ticket_exemption_quantity * exemptionPrice) + (ticket_group_quantity * groupPrice),
+        user_id: users[0].user_id,
+        equal_price:
+            (ticket_adult_quantity * adultPrice) +
+            (ticket_kid_quantity * kidPrice) +
+            (ticket_exemption_quantity * exemptionPrice) +
+            (ticket_group_quantity * groupPrice) +
+            (ticket_type1_quantity * type1Price) +
+            (ticket_type2_quantity * type2Price),
         created: new Date().toString()
     }
     console.log(order)
@@ -102,11 +178,15 @@ function buyTickets() {
             <td>${order.ticket_exemption_quantity}</td>
             <td>${order.ticket_group_price}</td>
             <td>${order.ticket_group_quantity}</td>
+            <td>${order.ticket_type1_price}</td>
+            <td>${order.ticket_type1_quantity}</td>
+            <td>${order.ticket_type2_price}</td>
+            <td>${order.ticket_type2_quantity}</td>
             <td>${order.barcode}</td>
+            <td>${order.user_id}</td>
             <td>${order.equal_price}</td>
             <td>${order.created}</td>
         </tr>`
     setStorageOrders();
-    console.log(Orders)
 }
 
